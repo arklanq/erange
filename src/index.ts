17ea69318dc2e84618e28cbae71168f279b4cd90
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-10-20 18:19:45 +0200

    fix: missing export in entry file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {type Registry} from './core/registry/Registry.js';
 export {Container, type ContainerCreationOptions, container} from './core/Container.js';
-export type {Provider, ClassProvider, InstanceProvider, FactoryProvider} from './core/provider/Provider.js';
+export type {Provider, ClassProvider, InstanceProvider, FactoryProvider, AliasProvider} from './core/provider/Provider.js';
 export {ProviderResolutionException} from './exceptions/ProviderResolutionException.js';
 export {InvalidProviderSigantureException} from './exceptions/InvalidProviderSigantureException.js';
 export {ClassInstanceInitializationException} from './exceptions/ClassInstanceInitializationException.js';
